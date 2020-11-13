ad373a4adbd37d1432d9df840ab48a540c900c71
souvikdas <Souvik.Das@dal.ca>
2020-11-13 19:55:59 -0400

    Removing Comments and Unnecessary RawDbConnection.py file as connections are configured in all the files.

--- src/MapReduceDataCollectionToALocalFile.py
+++ src/MapReduceDataCollectionToALocalFile.py
@@ -1,21 +1,19 @@
 import pymongo
 
-
 client = pymongo.MongoClient("mongodb+srv://root:root@datacluster.j83b5.mongodb.net/?retryWrites=true&w=majority")
 dataToBeCheckedList = []
 
 
 def fetchMongoDBProcessData(collection):
-    results = list(collection.find({}, {"_id": 0}))
+    results = list(collection.find({}, {"_id": 0, "created_at": 1, "username": 1, "location": 1, "tweet": 1, "likes": 1,
+                                        "followers": 1, "following": 1}))
     for entry in results:
-        # print(entry['tweet'])
         dataToBeCheckedList.append(entry['tweet'])
 
 
 def fetchMongoDBReuterData(collection):
-    results = list(collection.find({}, {"_id": 0}))
+    results = list(collection.find({}, {"_id": 0, "Date": 1, "News_Title": 1, "News": 1}))
     for entry in results:
-        # print(entry['News'])
         dataToBeCheckedList.append(entry['News'])
 
 
@@ -31,5 +29,3 @@ fetchMongoDBProcessData(processDb)
 fetchMongoDBReuterData(newsOne)
 fetchMongoDBReuterData(newsTwo)
 storeTheWordsInLocalFile()
-
-
--- src/RawDbConnection.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-
-client = pymongo.MongoClient(
-    "mongodb+srv://root:root@datacluster.j83b5.mongodb.net/RawDb?retryWrites=true&w=majority")
-db = client.RawDb
